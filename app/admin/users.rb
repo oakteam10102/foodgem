35775261dcc19d151727be5cb98397581398e6cb
Taufiq Muhammadi <waruboy@yahoo.com>
2014-09-13 06:39:08 +0700

    add paid referrals status

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register User do
-  actions :index, :show
+  actions :index, :show, :edit, :update
+
+  form do |f|
+    f.inputs "Edit Customer" do
+      f.input :email
+      f.input :paid
+    end
+    f.actions
+  end
 end
